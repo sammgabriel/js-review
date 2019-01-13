2a4dd2fcd046166de44e53c402a769a08672893e
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-01-13 15:56:17 -0500

    Wrote the FizzBuzz program in which Fizz is printed to the console when a multiple of 3 appears, Buzz is printed when a multiple of 5 appears, and FizzBuzz is printed when a multiple of both 3 and 5 appears.

--- js-review.js
+++ js-review.js
@@ -1,1 +1,30 @@
 console.log("Introduction to JavaScript");
+
+function fizzBuzz() {
+
+    for (let i = 1; i <= 100; i++) {
+
+        if ((i % 3 === 0) && (i % 5 === 0)) {
+
+            console.log("FizzBuzz");
+        }
+
+        else if ((i % 3 === 0) && (i % 5 !== 0)) {
+
+            console.log("Fizz");
+        }
+
+        else if ((i % 5 === 0) && (i % 3 !== 0)) {
+
+            console.log("Buzz");
+        }
+
+        else {
+
+            console.log(i);
+        }
+
+    }
+}
+
+fizzBuzz();
